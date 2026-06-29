79a7575ebfd07200c80d388568f3122916ab8a42
abraheemcoder <abraheem22468@gmail.com>
2026-06-29 15:51:45 +0500

    changes in JS Assignment (solve chapter_35 to 38 (Function) Extra Question)

---
+++ JavaScript_Assignments/Chapter_21 to 40/Chapter_35 to 38 (Function)/Function (Extra Question)/app.js
@@ -0,0 +1,74 @@
+// Function (Extra Question)
+
+// ================================
+// Q1
+// Create a function that prints:
+// Output: Hello JavaScript.
+// Answer:
+function greetJS() {
+    console.log("Hello JavaScript.");
+}
+greetJS();
+// ================================
+
+// ================================
+// Q2
+// Create a function that prints:
+// Output: I am learning JavaScript.
+// Answer:
+function learnJS() {
+    console.log("I am learning JavaScript.");
+}
+learnJS();
+// ================================
+
+// ================================
+// Q3
+// Create a function named welcome.
+// Output: Welcome to my website!
+// Answer:
+function welcome() {
+    console.log("Welcome to my website!");
+}
+welcome();
+// ================================
+
+// ================================
+// Q4
+// Create a function named bye.
+// Output: Good Bye!
+// Answer:
+function bye() {
+    console.log("Good Bye!");
+}
+bye();
+// ================================
+
+// ================================
+// Q5
+// Create two different functions:
+// morning()
+// night()
+// Output: Good Morning
+//         Good Night
+// Answer:
+function morning() {
+    console.log("Good Morning");
+}
+morning();
+
+function night() {
+    console.log("Good night");
+}
+night();
+// ================================
+// Q6
+// Create a program with these functions:
+// greet();
+// about();
+// contact();
+function greet() {
+    console.log("")
+}
+// Each function should print a different message, such as a simple website menu.
+// ================================
